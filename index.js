7d7877cb5fc3cad7e8eea389a2e63ef07df3ed9d
lokeshraj851134 <121623164+lokeshraj851134@users.noreply.github.com>
2023-01-20 02:10:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomnumber1=Math.floor(Math.random()*6+1);
 
-var randomimagesource1="dice" + randomnumber1 +".png";
+var randomimagesource1="/dicegame/dice" + randomnumber1 +".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomimagesource1);
 
 var randomnumber2=Math.floor(Math.random()*6+1);
-var randomimagesource2="dice" + randomnumber2 +".png";
+var randomimagesource2="/dicegame/dice" + randomnumber2 +".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomimagesource2);
 
 if (randomnumber1>randomnumber2){
